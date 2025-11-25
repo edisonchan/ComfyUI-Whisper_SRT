8d4e47cfcd0a7c28c68aebf0a955c51940a46eba
edisonchan <edison_chan_gz@hotmail.com>
2025-11-25 16:41:34 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,12 +2,11 @@ from .apply_whisper import ApplyWhisperNode
 from .add_subtitles_to_frames import AddSubtitlesToFramesNode
 from .add_subtitles_to_background import AddSubtitlesToBackgroundNode
 from .resize_cropped_subtitles import ResizeCroppedSubtitlesNode
-from .save_srt import SaveSRTNode, PreviewSRTNode
+from .save_srt import SaveSRTNode  # 只导入 SaveSRTNode
 
 NODE_CLASS_MAPPINGS = { 
     "Apply Whisper": ApplyWhisperNode,
     "Save SRT": SaveSRTNode,
-    "Preview SRT": PreviewSRTNode,
     "Add Subtitles To Frames": AddSubtitlesToFramesNode,
     "Add Subtitles To Background": AddSubtitlesToBackgroundNode,
     "Resize Cropped Subtitles": ResizeCroppedSubtitlesNode
@@ -16,7 +15,6 @@ NODE_CLASS_MAPPINGS = {
 NODE_DISPLAY_NAME_MAPPINGS = {
     "Apply Whisper": "Apply Whisper", 
     "Save SRT": "Save SRT",
-    "Preview SRT": "Preview SRT",
     "Add Subtitles To Frames": "Add Subtitles To Frames",
     "Add Subtitles To Background": "Add Subtitles To Background",
     "Resize Cropped Subtitles": "Resize Cropped Subtitles"
